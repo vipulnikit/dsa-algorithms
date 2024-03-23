0f6cddb1402c3edaab3b30960c12636af4fd57e2
Vipul Kumar <vipvipul1@gmail.com>
2024-03-23 18:09:20 +0530

    Kadane

--- Arrays/Kadane.java
+++ Arrays/Kadane.java
@@ -1,9 +1,17 @@
 public class Kadane {
     public static int solve(int[] A) {
-        return 0;
+        int N = A.length, ans = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i];
+            ans = Math.max(ans, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+        return ans;
     }
-    
+
     public static void main(String[] args) {
-        
+        int[] A = { 2, -2, -1, 5, -1, -2, 3, 8 };
+        System.out.println(solve(A));
     }
 }
